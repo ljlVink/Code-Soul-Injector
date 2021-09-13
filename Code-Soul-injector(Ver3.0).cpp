c4a75b7ee864f4c8a7c9880e2e1026240747f1a2
ljlVink <46098706+ljlVink@users.noreply.github.com>
2021-09-13 20:44:11 +0800

    Update Code-Soul-injector(Ver3.0).cpp

--- Code-Soul-injector(Ver3.0).cpp
+++ Code-Soul-injector(Ver3.0).cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
-string d[10000];
+string d[1000000+500];
 char h=39;
 int cv=0;
 bool a=1,kg=1,jing=0;
@@ -13,6 +13,7 @@ void injectingSoul(){
     cout<<" /*";
     for(int i=1;rand()%20;i++){
         int determine=rand()%5;
+        
         if(determine!=3){
             int s=rand()%126;
             if(s<33||char(s)=='/'||char(s)=='*'){continue;}
@@ -26,16 +27,19 @@ void injectingSoul(){
         if(determine==3){
             int s=rand()%126;
             if(s<33||char(s)=='/'||char(s)=='*'){continue;}
-                        int nnnnnnn=rand()%100+50;
+            	int nnnnnnn=rand()%100+50;
             if(nnnnnnn==82&&cv<=150){
                 cv++;
             cout<<endl;
             }
             int szzz=27;
-            cout<<'"'<<Vans[rand()%szzz-1]<<'"';
+            int pd=rand()%szzz-1;
+            if(pd==-1){
+            	pd=0;
+			} 
+            cout<<'"'<<Vans[pd]<<'"';
             cout<<char(s);
         }
-
     }
     cout<<" */";
 }
@@ -48,18 +52,19 @@ int main(int argc,char **argv){
     while(getline(cin,d[cnt]))cnt++;
     for(int i=0;i<cnt;i++){
         int sz=d[i].size();
+        
         a=1,kg=0,jing=0;
         for(int j=0;j<sz;j++){
             if(d[i][j]=='#'){jing=1;}
             if(d[i][j]==h||d[i][j]=='"'){
                 a=0;kg=1;
             }
-            if(d[i][j]==' '){
+            else if(d[i][j]==' '){
                 if(a==0&&kg==1){cout<<d[i][j];}
                 if(a==1)injectingSoul();
                 continue;
             }
-            if(d[i][j]=='{'||d[i][j]=='}'||d[i][j]=='('||d[i][j]==')'){
+            else if(d[i][j]=='{'||d[i][j]=='}'||d[i][j]=='('||d[i][j]==')'){
                 if(a==1)injectingSoul();
                 cout<<d[i][j];
                 continue;
@@ -69,5 +74,7 @@ int main(int argc,char **argv){
         if(jing==1){
             cout<<endl;
         }
+        
     }
+    return 0;
 }
